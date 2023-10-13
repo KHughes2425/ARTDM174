828970ae777530b95dd20a1a731bce8de10d0dc3
Kim <khughes949@insite.4cd.edu>
2023-10-12 18:50:36 -0700

    completed java

--- LAB-5/java/java.js
+++ LAB-5/java/java.js
@@ -1,29 +1,43 @@
-function autoSlide(){
-  setInterval(() =>{
-    slide(getItemActiveIndex() + 1);
-  }, 1000);
+window.addEventListener("load", () => {
+  autoSlide();
+})
+
+function autoSlide() {
+  setInterval(() => {
+     slide(getItemActiveIndex() + 1);
+  }, 2000); // slide speed = 3s
 }
 
-function slide(toIndex){
+function slide(toIndex) {
   const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
-  const itemsActive = document.querySelector(".carousel_item_active");
+  const itemActive = document.querySelector(".carousel_item__active");
 
-  if (toIndex > itemsArray.length){
-    toIndex = 0;
+  // check if toIndex exceeds the number of carousel items
+  if (toIndex >= itemsArray.length) {
+     toIndex = 0;
   }
+
+  const newItemActive = itemsArray[toIndex];
+
+  // start transition
+  newItemActive.classList.add("carousel_item__pos_next");
+  setTimeout(() => {
+     newItemActive.classList.add("carousel_item__next");
+     itemActive.classList.add("carousel_item__next");
+  }, 20);
+
+  // remove all transition class and switch active class
+  newItemActive.addEventListener("transitionend", () => {
+     itemActive.className = "carousel_item";
+     newItemActive.className = "carousel_item carousel_item__active";
+  }, {
+     once: true
+  });
 }
 
-const newItemsActive = itemsArray[toIndex];
-
-newItemsActive.classList.add("carousel_item_pos_next");
-setTimerout(() => {
-  newItemActive.classList.add("carousel_item_pos_next");
-  itemActive.classList.add("carousel_item_pos_next");
-}, 20);
-
-function getItemActiveIndex(){
-  const itemsArray = Array.from(document,querySelectorAll(".carousel_item"));
-  const itemActive = document.querySelector(".carousel_item_active");
+function getItemActiveIndex() {
+  const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
+  const itemActive = document.querySelector(".carousel_item__active");
   const itemActiveIndex = itemsArray.indexOf(itemActive);
-  return itemsActiveIndex;
+  return itemActiveIndex;
 }
