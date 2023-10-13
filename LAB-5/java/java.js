b7300b5272f6e8c4f2d3d2f23a16af6fc6ef7145
Kim <khughes949@insite.4cd.edu>
2023-10-12 22:37:57 -0700

    ...

--- LAB-5/java/java.js
+++ LAB-5/java/java.js
@@ -1,43 +1,19 @@
-window.addEventListener("load", () => {
-  autoSlide();
-})
+let slideIndex = 0;
+showSlides();
 
-function autoSlide() {
-  setInterval(() => {
-     slide(getItemActiveIndex() + 1);
-  }, 2000); // slide speed = 3s
-}
-
-function slide(toIndex) {
-  const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
-  const itemActive = document.querySelector(".carousel_item__active");
-
-  // check if toIndex exceeds the number of carousel items
-  if (toIndex >= itemsArray.length) {
-     toIndex = 0;
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
-
-  const newItemActive = itemsArray[toIndex];
-
-  // start transition
-  newItemActive.classList.add("carousel_item__pos_next");
-  setTimeout(() => {
-     newItemActive.classList.add("carousel_item__next");
-     itemActive.classList.add("carousel_item__next");
-  }, 20);
-
-  // remove all transition class and switch active class
-  newItemActive.addEventListener("transitionend", () => {
-     itemActive.className = "carousel_item";
-     newItemActive.className = "carousel_item carousel_item__active";
-  }, {
-     once: true
-  });
-}
-
-function getItemActiveIndex() {
-  const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
-  const itemActive = document.querySelector(".carousel_item__active");
-  const itemActiveIndex = itemsArray.indexOf(itemActive);
-  return itemActiveIndex;
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
